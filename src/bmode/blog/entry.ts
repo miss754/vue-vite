a44f3dc0410e52fb69c39bd5c1947d577c4a8a89
xgh1055219 <1119391382@qq.com>
2022-08-21 18:43:26 +0800

    #业务模块-语言包初始化

--- src/bmode/blog/entry.ts
+++ src/bmode/blog/entry.ts
@@ -2,7 +2,7 @@
  * @Author: xgh1055219 1119391382@qq.com
  * @Date: 2022-08-21 18:39:28
  * @LastEditors: xgh1055219 1119391382@qq.com
- * @LastEditTime: 2022-08-21 18:39:47
+ * @LastEditTime: 2022-08-21 18:41:03
  * @FilePath: \typescriptd:\CodeHub\vue-vite\src\bmode\blog\entry.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
